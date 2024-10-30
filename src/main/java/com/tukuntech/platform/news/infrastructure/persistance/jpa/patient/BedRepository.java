525b90a3ea4301011065d17bfb6f6b1ba01accaf
Oscar <oscaredu01@hotmail.com>
2024-10-30 16:08:38 -0500

    chore: rename project to tukun.

--- src/main/java/com/tukuntech/platform/news/infrastructure/persistance/jpa/patient/BedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tukuntech.platform.news.infrastructure.persistance.jpa.patient;
-
-import com.tukuntech.platform.news.domain.model.aggregates.patient.Bed;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BedRepository extends JpaRepository<Bed, Long> {
-}
--- src/main/java/com/tukuntech/platform/news/infrastructure/persistance/jpa/patient/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tukuntech.platform.news.infrastructure.persistance.jpa.patient;
-
-import com.tukuntech.platform.news.domain.model.aggregates.patient.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
