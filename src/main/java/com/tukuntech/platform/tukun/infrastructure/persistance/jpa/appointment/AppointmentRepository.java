8f644a4ac424bb10a15bcd67f379ed004f54dea0
Oscar <oscaredu01@hotmail.com>
2024-10-30 20:12:05 -0500

    fix: remove folders to resolve errors in the project

--- src/main/java/com/tukuntech/platform/tukun/infrastructure/persistance/jpa/appointment/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tukuntech.platform.tukun.infrastructure.persistance.jpa.appointment;
-
-import com.tukuntech.platform.tukun.domain.model.aggregates.patient.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointmentRepository extends JpaRepository <Appointment, Long> {
-}
