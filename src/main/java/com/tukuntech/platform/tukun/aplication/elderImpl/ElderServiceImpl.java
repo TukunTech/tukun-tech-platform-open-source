75a11fe989bf96093f4038b8281fa40d26af1ca9
Ericksnpaii <erickmaraza20@gmail.com>
2024-11-01 20:58:18 -0500

    feat: add ElderServiceImpl with ElderService implementation

--- src/main/java/com/tukuntech/platform/tukun/aplication/elderImpl/ElderServiceImpl.java
+++ src/main/java/com/tukuntech/platform/tukun/aplication/elderImpl/ElderServiceImpl.java
@@ -6,7 +6,7 @@ import com.tukuntech.platform.tukun.infrastructure.persistance.jpa.elder.ElderRe
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-
+//Elder
 @Service
 public class ElderServiceImpl implements ElderService {
 
