36729396392aa3aed77863b22813d09827d54010
Fabio <U202218791@gmail.com>
2024-11-03 03:08:44 -0500

    feat(emergency-numbers): define EmergencyNumbersService interface for emergency numbers logic

---
+++ src/main/java/com/tukuntech/platform/tukun/domain/services/emergencyNumbers/EmergencyNumbersService.java
@@ -0,0 +1,13 @@
+package com.tukuntech.platform.tukun.domain.services.emergencyNumbers;
+
+import com.tukuntech.platform.tukun.domain.model.aggregates.emergencyNumbers.EmergencyNumbers;
+
+import java.util.List;
+
+public interface EmergencyNumbersService {
+    List<EmergencyNumbers> GetAllEmergencyNumbers();
+    public abstract EmergencyNumbers UpdateAndSaveEmergencyNumbers(EmergencyNumbers emergencyNumbers);
+    public abstract void DeleteEmergencyNumbers(Long emergencyNumbersId);
+
+    List<EmergencyNumbers> GetAllEmergencyNumbersById(int id);
+}
