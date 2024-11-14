9207f5c06093c849fa493212fdba66f0582de8ae
Oscar <oscaredu01@hotmail.com>
2024-11-13 19:03:32 -0500

    fix(api): correct route naming conventions for `appointment` endpoint

--- src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/AppointmentController.java
+++ src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/AppointmentController.java
@@ -2,6 +2,10 @@ package com.tukuntech.platform.tukun.interfaces.rest.transform;
 
 import com.tukuntech.platform.tukun.domain.model.aggregates.appointment.Appointment;
 import com.tukuntech.platform.tukun.domain.services.appointment.AppointmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,21 +14,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
-@RequestMapping("/appointment/v1")
+@RequestMapping(value = "/api/v1/appointments", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Appointment", description = "Available Appointment Endpoints")
 public class AppointmentController {
 
     @Autowired
     private AppointmentService appointmentService;
 
     @GetMapping
+    @Operation(summary = "Get all appointments", description = "Get all appointments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Appointments found"),
+            @ApiResponse(responseCode = "404", description = "Appointments not found")
+    })
     @ResponseBody
     public ResponseEntity<List<Appointment>> GetAll(){
         List<Appointment> list = appointmentService.GetAllAppointments();
         return ResponseEntity.ok(list);
     }
 
-    @PostMapping("/createAppointment")
+    @PostMapping
+    @Operation(summary = "Create a new appointment", description = "Create a new appointment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Appointment created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> CreateAppointment(@RequestBody Appointment appointment){
         Map<String, Object> response = new HashMap<>();
@@ -39,7 +56,12 @@ public class AppointmentController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/updateAppointment")
+    @PutMapping("/{appointmentId}")
+    @Operation(summary = "Update an appointment", description = "Update an appointment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Appointment updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> UpdateAppointment(@RequestBody Appointment appointment){
         Map<String, Object> response = new HashMap<>();
@@ -53,7 +75,12 @@ public class AppointmentController {
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/deleteAppointment/{id}")
+    @DeleteMapping("/{appointmentId}")
+    @Operation(summary = "Delete an appointment", description = "Delete an appointment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Appointment deleted"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> DeleteAppointment(@PathVariable("id") long id){
         Map<String, Object> response = new HashMap<>();
