2032b5862a9a76a04eec08bb5677dad9254d24ab
adri <adriana03pch@gmail.com>
2024-11-15 10:58:26 -0500

    feat: update bed and patient routes

--- src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/BedController.java
+++ src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/BedController.java
@@ -3,6 +3,10 @@ package com.tukuntech.platform.tukun.interfaces.rest.transform;
 
 import com.tukuntech.platform.tukun.domain.model.aggregates.patient.Bed;
 import com.tukuntech.platform.tukun.domain.services.patient.BedService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +15,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 @RestController
-@RequestMapping( "/v1/bed")
+@RequestMapping(value = "/api/v1/bed", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Bed", description = "Available Bed Endpoints")
 public class BedController {
 
     @Autowired
     private BedService bedService;
 
     @GetMapping
+    @Operation(summary = "Get all beds", description = "Get all beds")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Beds found"),
+            @ApiResponse(responseCode = "404", description = "Beds not found")
+    })
     @ResponseBody
     public ResponseEntity<List<Bed>> GetAll(){
         List<Bed> list = bedService.GetAllBed();
         return ResponseEntity.ok(list);
     }
 
-    @PostMapping("/create")
+    @PostMapping
+    @Operation(summary = "Create a new bed", description = "Create a new bed")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Bed created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> CreateBed(@RequestBody Bed bed){
         Map<String, Object> exit = new HashMap<>();
@@ -49,7 +66,13 @@ public class BedController {
     }
 
 
-    @PutMapping("/update")
+    @PutMapping("/{bedId}")
+    @Operation(summary = "Update a bed", description = "Update a bed")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Bed updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Bed not found")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> UpdateBed(@RequestBody Bed bed){
         Map<String, Object> exit = new HashMap<>();
@@ -70,7 +93,13 @@ public class BedController {
     }
 
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{bedId}")
+    @Operation(summary = "Delete a bed", description = "Delete a bed")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Bed deleted"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Bed not found")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> DeleteBed(@PathVariable("id") long id){
         Map<String, Object> exit = new HashMap<>();
--- src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/PatientController.java
+++ src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/PatientController.java
@@ -3,6 +3,10 @@ package com.tukuntech.platform.tukun.interfaces.rest.transform;
 
 import com.tukuntech.platform.tukun.domain.model.aggregates.patient.Patient;
 import com.tukuntech.platform.tukun.domain.services.patient.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,21 +15,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
-@RequestMapping( "/v1/patient")
+@RequestMapping(value = "/api/v1/patients", produces = APPLICATION_JSON_VALUE)
+@Tag(name = "Patient", description = "Available Patient Endpoints")
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
 
-    @GetMapping
+    @GetMapping("/patients")
+    @Operation(summary = "Get all patients", description = "Get all patients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patients found"),
+            @ApiResponse(responseCode = "404", description = "Patients not found")
+    })
     @ResponseBody  // Se utiliza para retornar datos Json
     public ResponseEntity<List<Patient>> GetAll(){
         List<Patient> list = patientService.GetAllPatients();
         return ResponseEntity.ok(list);
     }
 
-    @PostMapping("/create")
+    @PostMapping
+    @Operation(summary = "Create a new patients", description = "Create a new patients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Patients created"),
+            @ApiResponse(responseCode = "400", description = "Invalid input")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> CreatePatient(@RequestBody Patient patient){
         Map<String, Object> exit = new HashMap<>();
@@ -46,7 +63,13 @@ public class PatientController {
     }
 
 
-    @PutMapping ("/update") //IMPORTANTE PONERLO PUT PARA ACTUALIZAR
+    @PutMapping ("/{patientId}") //IMPORTANTE PONERLO PUT PARA ACTUALIZAR
+    @Operation(summary = "Update a patient", description = "Update a patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient updated"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Patient not found")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> UpdatePatient(@RequestBody Patient patient){
         Map<String, Object> exit = new HashMap<>();
@@ -66,7 +89,13 @@ public class PatientController {
     }
 
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{patientId}")
+    @Operation(summary = "Delete a patient", description = "Delete a patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Patient deleted"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+            @ApiResponse(responseCode = "404", description = "Patient not found")
+    })
     @ResponseBody
     public ResponseEntity<Map<String, Object>> DeletePatient(@PathVariable("id") long id){
         Map<String, Object> exit = new HashMap<>();
