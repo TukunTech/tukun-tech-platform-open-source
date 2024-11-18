2b25fd92e2a18574a8d90170b4dd0665ece2e862
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-18 17:10:11 -0500

    feat(type-alert): define service interface for TypeAlert operations

--- src/main/java/com/tukuntech/platform/tukun/domain/services/criticalAlerts/TypeAlertService.java
+++ src/main/java/com/tukuntech/platform/tukun/domain/services/criticalAlerts/TypeAlertService.java
@@ -1,7 +1,9 @@
 package com.tukuntech.platform.tukun.domain.services.criticalAlerts;
 
+import com.tukuntech.platform.tukun.domain.model.aggregates.criticalAlerts.TypeAlert;
+
 import java.util.List;
 
 public interface TypeAlertService {
-    List<TypeAlertService> GetAllTypeAlerts();
+    List<TypeAlert> GetAllTypeAlerts();
 }
