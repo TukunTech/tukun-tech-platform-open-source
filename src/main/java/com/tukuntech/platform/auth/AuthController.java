c2319d1701c43730462ef4f3d5077ed922ba5100
Oscar <oscaredu01@hotmail.com>
2024-11-18 19:23:51 -0500

    feat(auth): implement AuthController for user authentication and registration

---
+++ src/main/java/com/tukuntech/platform/auth/AuthController.java
@@ -0,0 +1,28 @@
+package com.tukuntech.platform.auth;
+
+import com.tukuntech.platform.tukun.domain.util.appSetings;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/v1/auth", produces = APPLICATION_JSON_VALUE) // Alinear con SecurityConfig
+@CrossOrigin(origins = appSetings.URL_CROSS_ORIGIN) // Configuración de CORS
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping(value = "/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+}
