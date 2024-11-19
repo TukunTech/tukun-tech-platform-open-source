2adf2ff0b837d35d19c245c1912a1786950b552e
Rafooo6 <rafaelbravo032@gmail.com>
2024-11-18 21:39:24 -0500

    feat(Type-Alerts): add controller and routes.

---
+++ src/main/java/com/tukuntech/platform/tukun/interfaces/rest/transform/TypeAlertsController.java
@@ -0,0 +1,28 @@
+package com.tukuntech.platform.tukun.interfaces.rest.transform;
+
+import com.tukuntech.platform.tukun.domain.model.aggregates.criticalAlerts.TypeAlert;
+import com.tukuntech.platform.tukun.domain.model.aggregates.technicalSupport.TechnicalSupport;
+import com.tukuntech.platform.tukun.domain.services.criticalAlerts.TypeAlertService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*") // Configura los orígenes permitidos según tu necesidad
+@RequestMapping("/api/v1/typeAlerts")
+public class TypeAlertsController {
+    @Autowired
+    private TypeAlertService typeAlertService;
+
+    @GetMapping
+    @ResponseBody
+    public ResponseEntity<List<TypeAlert>> GetAllTypeAlerts(){
+        List<TypeAlert> list = typeAlertService.GetAllTypeAlerts();
+        return ResponseEntity.ok(list);
+    }
+
+
+
+}
